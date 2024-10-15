55b5a14c78c376412ef279b9773a26e7bbe9e7a3
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-14 21:13:07 -0500

    Listar Estudiantes

---
+++ Codigo2024Clase18/frmEstudiante.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Codigo2024Clase18
+{
+    public partial class frmEstudiante : Form
+    {
+        //BindingList
+        List<Estudiante> estudiantes = new List<Estudiante>();
+        public frmEstudiante()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nombres = txtNombres.Text;
+                string apellidos = txtApellidos.Text;
+                DateTime fechaMatricula = dtpFechaMatricula.Value;
+                bool esBecado = chkBecado.Checked;
+
+                estudiantes.Add(new Estudiante
+                {
+                    EstudianteID=estudiantes.Count+1,
+                    Apellidos = apellidos,
+                    Nombres = nombres,
+                    EsBecado = esBecado,
+                    FechaMatricula = fechaMatricula
+                });
+               
+
+
+                MessageBox.Show("Registro Exitoso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Comunicarse con el administrador");
+                //throw ex;
+            }
+           
+            //dgvEstudiantes.Rows.Add( nombres,apellidos,fechaMatricula,esBecado);
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            dgvEstudiantes.DataSource = null;
+            
+            dgvEstudiantes.DataSource = estudiantes;
+        }
+    }
+}
